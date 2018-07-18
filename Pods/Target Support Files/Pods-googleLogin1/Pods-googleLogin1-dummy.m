c8dd173c372e700f662d4c28c090cdeb4cbbf642
hamasugar <hamazi511@gmail.com>
2018-07-19 03:22:48 +0900

    特になし

---
+++ Pods/Target Support Files/Pods-googleLogin1/Pods-googleLogin1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_googleLogin1 : NSObject
+@end
+@implementation PodsDummy_Pods_googleLogin1
+@end
--- googleLogin1/ViewController.swift
+++ googleLogin1/ViewController.swift
@@ -7,14 +7,70 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController,GIDSignInDelegate,GIDSignInUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let button = GIDSignInButton()
+        button.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        self.view.addSubview(button)
+        
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        
     }
 
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+    
+        if let aa = error{
+            print ("errordesu")
+            
+            return
+        }
+        
+        print ("era-nasi")
+        
+        guard let idToken = user.authentication.idToken else{
+            
+            return
+        }
+        
+        guard let accessToken = user.authentication.accessToken else{
+            
+            return
+        }
+        
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+        
+        Auth.auth().signIn(with: credential, completion: {  (user, error) in
+            
+            if let err = error{
+                
+                print (err)
+                return
+            }
+            
+            print ("seikou")
+            
+            
+            self.performSegue(withIdentifier: "next", sender: nil)
+            
+            
+            })
+        
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
